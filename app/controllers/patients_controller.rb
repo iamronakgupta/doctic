a091ab4d37c62d035dc93194a3355e058c631d86
Ronak Gupta <iamronakgupta@gmail.com>
2024-06-13 22:12:02 +0530

    patient listed on index page

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,7 @@
 class PatientsController < ApplicationController
 
   def index
+    @patients = current_user.patients
   end
 
   def new
--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,11 @@
 module PatientsHelper
+  def appointment(patient)
+    "#{patient.appointment_date} at #{patient.appointment_time.strftime("%I:%M%p")
+    }"
+  end
+
+  def active_or_inactive(patient)
+    return "Active" if patient.appointment_active
+    "Inactive"
+  end
 end
