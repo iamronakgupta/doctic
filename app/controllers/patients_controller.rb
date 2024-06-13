7ccbd49e7c59b027d81234aebd32ab14922da02c
Ronak Gupta <iamronakgupta@gmail.com>
2024-06-13 21:46:52 +0530

    new patient created added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,28 @@ class PatientsController < ApplicationController
 
   def index
   end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    @patient.receptionist_type = "User"
+    @patient.doctor_type = "User"
+    @patient.receptionist = current_user if current_user.type = "Receptionist"
+    @patient.appointment_active = true
+
+    if @patient.save
+      redirect_to patients_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def patient_params
+      params.require(:patient).permit(:name, :phone, :disease, :doctor_id,
+        :receptionist_id, :appointment_date, :appointment_time)
+    end
 end
